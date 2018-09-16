1d370ed28fc5f960368880fddc3b6a70530d947e
WangAo <908622977@qq.com>
2018-09-16 17:11:05 +0800

    添加检索配置

--- src/main/java/priv/wangao/LogAnalysis/util/EsHelper.java
+++ src/main/java/priv/wangao/LogAnalysis/util/EsHelper.java
@@ -5,6 +5,7 @@ import java.net.UnknownHostException;
 import java.util.ArrayList;
 import java.util.List;
 
+import org.elasticsearch.action.search.SearchRequestBuilder;
 import org.elasticsearch.action.search.SearchResponse;
 import org.elasticsearch.client.transport.TransportClient;
 import org.elasticsearch.common.settings.Settings;
@@ -15,12 +16,29 @@ import org.elasticsearch.index.query.QueryBuilders;
 import org.elasticsearch.search.SearchHit;
 import org.elasticsearch.search.sort.SortOrder;
 
+/**
+ * @author WangAo
+ *
+ */
 public class EsHelper {
 
+	// client : Es客户端
 	private TransportClient client = null;
+	// cluster : 集群名称
 	private String cluster = null;
+	// addrs : 集群地址数组
 	private List<String> addrs = new ArrayList<String>();
 
+	/**
+	 * @Title:EsHelper
+	 * @Description: 指定集群名称和集群地址构建客户端的构造函数
+	 * @param cluster
+	 *            集群名称
+	 * @param addrs
+	 *            集群地址
+	 * @throws Exception
+	 *             集群地址无效时，抛出异常
+	 */
 	public EsHelper(String cluster, String addrs) throws Exception {
 		// TODO Auto-generated constructor stub
 		String[] split = addrs.split(";");
@@ -41,6 +59,14 @@ public class EsHelper {
 		this.client = this.getClientInstance();
 	}
 
+	/**
+	 * @Title: getClientInstance
+	 * @Description: 获取ES客户端实例
+	 * @return ES客户端实例
+	 * @throws NumberFormatException
+	 * @throws UnknownHostException
+	 * @return: TransportClient
+	 */
 	private TransportClient getClientInstance() throws NumberFormatException, UnknownHostException {
 		if (this.client == null) {
 			Settings settings = Settings.builder().put("cluster.name", cluster).build();
@@ -61,42 +87,108 @@ public class EsHelper {
 		return this.client;
 	}
 
-	public void close() {
-		this.client.close();
+	/**
+	 * @Title: getRequestBuilderv24
+	 * @Description: 获取请求体对象
+	 * @param index
+	 *            目标索引
+	 * @param includes
+	 *            需要的field数组
+	 * @param excludes
+	 *            不要的field数组
+	 * @param qb
+	 *            请求体对象
+	 * @param batchSize
+	 *            批量查询尺寸
+	 * @param ttlSeconds
+	 *            连接过期时间
+	 * @return
+	 * @return: SearchRequestBuilder
+	 */
+	private SearchRequestBuilder getRequestBuilderv24(String index, String[] includes, String[] excludes,
+			QueryBuilder qb, int batchSize, int ttlSeconds) {
+		SearchRequestBuilder requestBuilder = this.client.prepareSearch(index);
+
+		requestBuilder.setFetchSource(includes, excludes);
+
+		requestBuilder.addSort("_doc", SortOrder.ASC).setScroll(TimeValue.timeValueSeconds(ttlSeconds)).setQuery(qb)
+				.setSize(batchSize);
+
+		return requestBuilder;
 	}
 
+	/**
+	 * @Title: getMatchAllQuery
+	 * @Description: 获取match_all查询体对象
+	 * @return match_all查询体对象
+	 * @return: QueryBuilder
+	 */
 	private QueryBuilder getMatchAllQuery() {
 		return QueryBuilders.matchAllQuery();
 	}
 
-	private void scrollRequest(String index, QueryBuilder qb, int batchSize, int ttlSeconds) {
-		SearchResponse scrollResp = this.client.prepareSearch(index)
-				// .addSort(FieldSortBuilder.DOC_FIELD_NAME, SortOrder.ASC)
-				.addSort("_doc", SortOrder.ASC).setScroll(TimeValue.timeValueSeconds(ttlSeconds)).setQuery(qb)
-				.setSize(batchSize).get();
-		
+	/**
+	 * @Title: scrollRequest
+	 * @Description: 执行滚动查询
+	 * @param index
+	 *            目标索引名
+	 * @param includes
+	 *            需要的field数组
+	 * @param excludes
+	 *            不要的field数组
+	 * @param qb
+	 *            查询体对象
+	 * @param batchSize
+	 *            批量查询大小
+	 * @param ttlSeconds
+	 *            超时时间
+	 * @param maxCount
+	 *            设定最大获取数量，超过即停止获取
+	 * @param targetPath
+	 *            输出的目标路径
+	 * @return: void
+	 */
+	private void scrollRequest(String index, String[] includes, String[] excludes, QueryBuilder qb, int batchSize,
+			int ttlSeconds, int maxCount, String targetPath) {
+		SearchResponse scrollResp = this.getRequestBuilderv24(index, includes, excludes, qb, batchSize, ttlSeconds)
+				.get();
+
 		int count = 0;
 
 		do {
 			for (SearchHit hit : scrollResp.getHits().getHits()) {
-				// Handle the hit...
-				System.out.println(hit.getSource().get("message"));
+				System.out.println(hit.getSourceAsString());
+				if (targetPath != null) {
+					IOHelper.getInstance().writeToFile(hit.getSourceAsString() + "\r\n", targetPath, true);
+				}
 				count++;
+				if (maxCount > 0 && count >= maxCount)
+					break;
 			}
-
+			if (maxCount > 0 && count >= maxCount)
+				break;
 			scrollResp = client.prepareSearchScroll(scrollResp.getScrollId())
 					.setScroll(TimeValue.timeValueSeconds(ttlSeconds)).execute().actionGet();
 		} while (scrollResp.getHits().getHits().length != 0 && count < 100000);
 	}
 
-	public void matchAllQuery(String index) {
-		this.scrollRequest(index, this.getMatchAllQuery(), 1000, 60);
+	/**
+	 * @Title: matchAllQuery
+	 * @Description: 对外暴露的match_all方法查询
+	 * @param index
+	 *            目标索引名
+	 * @param targetPath
+	 *            目标路径
+	 * @return: void
+	 */
+	public void matchAllQuery(String index, String[] includes, String[] excludes, String targetPath) {
+		this.scrollRequest(index, includes, excludes, this.getMatchAllQuery(), 1000, 60, 100000, targetPath);
 	}
 
 	public static void main(String[] args) throws Exception {
 		// TODO Auto-generated method stub
 		EsHelper esHelper = new EsHelper("nic-multi-logs", "10.1.1.201:9300");
-		esHelper.matchAllQuery("niclog-4th-2018.01.30");
+		esHelper.matchAllQuery("niclog-4th-2018.01.30", new String[] { "message" }, null, "target.txt");
 	}
 
 }
--- src/main/java/priv/wangao/LogAnalysis/util/IOHelper.java
+++ src/main/java/priv/wangao/LogAnalysis/util/IOHelper.java
@@ -1,11 +1,53 @@
 package priv.wangao.LogAnalysis.util;
 
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.OutputStreamWriter;
+import java.util.List;
+
 public class IOHelper {
 	
 	private static IOHelper instance = new IOHelper();
 	
 	public static IOHelper getInstance() {
 		return instance;
+	}
+	
+	public void writeToFile(String context, String filePath, boolean append) {
+		try(
+			FileOutputStream fos = new FileOutputStream(new File(filePath), append);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			BufferedWriter bw = new BufferedWriter(osw);
+		) {
+			bw.write(context);
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+	
+	public void writeToFile(List<String> context, String filePath, boolean append) {
+		try(
+			FileOutputStream fos = new FileOutputStream(new File(filePath), append);
+			OutputStreamWriter osw = new OutputStreamWriter(fos);
+			BufferedWriter bw = new BufferedWriter(osw);
+		) {
+			for (String line : context) {
+				bw.write(line);
+			}
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
 	}
 
 
